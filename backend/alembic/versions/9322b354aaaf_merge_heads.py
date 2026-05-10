7c52be095f651239c73945bc159b4f32df4601fa
v4rgas <66626747+v4rgas@users.noreply.github.com>
2026-05-10 00:20:13 -0400

    chore: merge alembic migration heads

---
+++ backend/alembic/versions/9322b354aaaf_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: 9322b354aaaf
+Revises: 5d2f43c6b1aa, b2c3d4e5f6a7
+Create Date: 2026-05-10 00:19:59.043505
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9322b354aaaf'
+down_revision: Union[str, Sequence[str], None] = ('5d2f43c6b1aa', 'b2c3d4e5f6a7')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
